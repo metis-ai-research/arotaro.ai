f79e3fd8e8c1148ecfaea7a35875bd140b494525
Mao Li <anrenyil@gmail.com>
2024-01-16 14:42:56 +0800

    second section of main

--- src/pages/Home/MainSection.js
+++ src/pages/Home/MainSection.js
@@ -1,6 +1,12 @@
 import "./MainSection.scss";
 import Man from "../../resources/home-man.png";
+import Lisa from "../../resources/Lisa.png";
+import map from "../../resources/map.png";
+import books from "../../resources/books.png";
+import password from "../../resources/password.png";
+
 import ButtonRow from "../../components/ButtonRow";
+import IconCard from "./IconCard";
 
 export default function MainSection() {
   return (
@@ -16,6 +22,32 @@ export default function MainSection() {
           <ButtonRow type="home-button" />
         </div>
         <img src={Man} alt="home-man" className="home-man" />
+      </div>
+      <div className="second-section">
+        <img src={Lisa} alt="home-woman" className="home-woman" />
+        <div className="card-section">
+          <h2 className="second-slogon">아로타로 ai는 뭐가 특별한가요?</h2>
+          <div className="icon-cards-container">
+            <IconCard
+              icon={map}
+              title="접근성"
+              className="map-icon"
+              content="당신이 필요한 순간, 언제든지 어디에서나 예약 없이도 바로 이용 가능합니다."
+            />
+            <IconCard
+              icon={books}
+              title="전문성"
+              className="books-icon"
+              content="방대한 데이터로 훈련받아 정교한 타로 리딩으로 당신의 고민을 상담 할 수 있습니다."
+            />
+            <IconCard
+              icon={password}
+              title="프라이빗"
+              className="password-icon"
+              content="다른 사람들에게 공유하기 힘든 고민이나 개인적인 이야기들을 할수있습니다."
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
