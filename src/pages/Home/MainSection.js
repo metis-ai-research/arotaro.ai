880b575d3526dd43b86fc9ef48a2e0ee2214bcb2
Mao Li <anrenyil@gmail.com>
2024-01-21 21:23:52 +0800

    first-section-mobile

--- src/pages/Home/MainSection.js
+++ src/pages/Home/MainSection.js
@@ -18,10 +18,16 @@ export default function MainSection() {
       <div className="first-section">
         <div className="greeting-container">
           <h1 className="greeting-slogon">
-            마음의 미로에서 길을 찾고 계신가요?
+            마음의
+            <br className="br-for-mobile" />
+            미로에서
+            <br className="br-for-mobile" />
+            길을 찾고 계신가요?
           </h1>
           <p className="greeting-content">
-            혼란스럽고 어려운 순간, 아로타로 AI가 당신의 길잡이가 되어드립니다.
+            혼란스럽고 어려운 순간, 아로타로 AI가
+            <br className="br-for-mobile" />
+            당신의 길잡이가 되어드립니다.
           </p>
           <ButtonRow type="home-button" />
         </div>
