f17baaa95d181e2c9d2c8c843d96e848c3554ee0
jazhaozhang <jazhaozhang@gmail.com>
2024-10-28 21:22:15 -0700

    add iOS download link

--- src/components/ButtonRow.js
+++ src/components/ButtonRow.js
@@ -3,21 +3,29 @@ import apple from "../resources/apple.png";
 import google from "../resources/google-play.png";
 
 export default function ButtonRow(props) {
-  const handleClick = () => {
+  const handleGoogleClick = () => {
     window.open(
       "https://play.google.com/store/apps/details?id=com.arotaro.android",
       "_blank",
     );
   };
+
+  const handleAppleClick = () => {
+    window.open(
+      "https://apps.apple.com/ca/app/arotaro/id6475332338?platform=iphone",
+      "_blank",
+    );
+  };
+
   return (
     <div className="button-row">
-      <MainButton type={props.type}>
+      <MainButton type={props.type} onClick={handleAppleClick}>
         <img src={apple} alt="apple-store" className="button-icon" />
-        Coming soon
+        App Store
       </MainButton>
-      <MainButton type={props.type} onClick={handleClick}>
+      <MainButton type={props.type} onClick={handleGoogleClick}>
         <img src={google} alt="google-play" className="button-icon" />
-        Google play
+        Google Play
       </MainButton>
     </div>
   );
